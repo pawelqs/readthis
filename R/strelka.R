368ec28eec14c7b404713e3120818c9d6b19cca1
Paweł <kpawel2210@gmail.com>
2023-07-19 14:41:43 +0200

    read_strelka_somatic_snvs() can read multiple files like read_mutect_snvs()

--- R/strelka.R
+++ R/strelka.R
@@ -4,24 +4,21 @@
 #'
 #' Reads the variant calls from Strelka2 Small Variant Caller
 #'
-#' @param file VCF file to read
-#' @param sample_id ID(s) of the tumor sample to use. The last ID will be used
-#'   if none provided
-#' @param overwrite_sample_id This argument can be specified to overwrite the
-#'   sample_id present in the VCF file
-#' @param PASS_only Keep FILTER == PASS variants only?
-#' @param chrom_convention UCSC/NCBI/keep
-#' @param verbose Verbose?
+#'
+#' @inheritParams mutect
 #'
 #' @examples
 #' library(readthis)
-#' path <- system.file("extdata", "Strelka", "S1.somatic.snvs.vcf.gz", package = "readthis")
-#' read_strelka_somatic_snvs(
-#'   path,
-#'   sample_id = "TUMOR",
-#'   overwrite_sample_id = "S1",
-#'   verbose = FALSE
-#' )
+#'
+#' file1 <- system.file("extdata", "Strelka", "S1.somatic.snvs.vcf.gz", package = "readthis")
+#' read_strelka_somatic_snvs(file1)
+#'
+#' file2 <- system.file("extdata", "Strelka", "S2.somatic.snvs.vcf.gz", package = "readthis")
+#' files <- c(S1 = file1, S2 = file2)
+#' read_strelka_somatic_snvs(files, verbose = FALSE)
+#'
+#' dir <- system.file("extdata", "Strelka", package = "readthis")
+#' read_strelka_somatic_snvs(dir)
 #'
 #' @name strelka
 NULL
@@ -30,22 +27,54 @@ NULL
 
 #' @rdname strelka
 #' @export
-read_strelka_somatic_snvs <- function(file,
-                                      sample_id = NULL,
-                                      overwrite_sample_id = NULL,
+read_strelka_somatic_snvs <- function(path,
+                                      sample_ids = "drop_first",
                                       PASS_only = TRUE,
+                                      patient_id_pattern = "(?<=\\/)[:alnum:]*(?=\\.)",
                                       chrom_convention = "UCSC",
                                       verbose = TRUE) {
+  if (is_single_file(path)) {
+    snvs <- read_strelka_vcf(path, sample_ids, PASS_only, verbose)
+  } else if (is_list_of_files(path)) {
+    snvs <- map(path, read_strelka_vcf, sample_ids, PASS_only, verbose) |>
+      bind_rows(.id = "patient_id")
+  } else if (is_single_dir(path)) {
+    files <- list.files(path, full.names = TRUE)
+    patient_ids <- files |>
+      str_extract(pattern = patient_id_pattern)
+    names(files) <- patient_ids
+    snvs <- files |>
+      map(read_strelka_vcf, sample_ids, PASS_only, verbose) |>
+      bind_rows(.id = "patient_id")
+  }
+
+  class(snvs) <- c("cevo_Strelka", class(snvs))
+  snvs |>
+    use_chrom_naming_convention(chrom_convention)
+}
+
+
+
+## ------------------------------ Functions ------------------------------------
+
+
+read_strelka_vcf <- function(file,
+                             sample_ids = NULL,
+                             # overwrite_sample_id = NULL,
+                             PASS_only = TRUE,
+                             verbose = TRUE) {
   tidyvcf <- read_vcf(file, PASS_only, verbose = verbose)
 
-  if (is.null(sample_id)) {
+  if (sample_ids == "drop_first") {
     sample_ids <- unique(tidyvcf$dat$Indiv)
-    sample_id <- last(sample_ids)
-    msg("Guessing sample_id: ", sample_id, verbose = verbose)
+    sample_ids <- sample_ids[-1]
+    msg("Guessing sample_ids: ", str_c(sample_ids, collapse = ", "), verbose = verbose)
+  } else if (sample_ids == "all") {
+    sample_ids <- unique(tidyvcf$dat$Indiv)
   }
 
   snvs <- tidyvcf$dat |>
-    filter(.data$Indiv %in% sample_id) |>
+    filter(.data$Indiv %in% sample_ids) |>
     calc_strelka_somatic_VAF() |>
     select(
       sample_id = "Indiv",
@@ -54,15 +83,13 @@ read_strelka_somatic_snvs <- function(file,
       ref_reads = "ref_tier1",
       alt_reads = "alt_tier1",
       VAF = "VAF", DP = "DP"
-    ) |>
-    use_chrom_naming_convention(chrom_convention)
+    )
 
-  if (!is.null(overwrite_sample_id)) {
-    snvs$sample_id <- overwrite_sample_id
-    msg("sample_id set to: ", sample_id, verbose = verbose)
-  }
+  # if (!is.null(overwrite_sample_id)) {
+  #   snvs$sample_id <- overwrite_sample_id
+  #   msg("sample_id set to: ", overwrite_sample_id, verbose = verbose)
+  # }
 
-  class(snvs) <- c("cevo_Strelka", class(snvs))
   snvs
 }
 
--- tests/testthat/test-strelka.R
+++ tests/testthat/test-strelka.R
@@ -13,10 +13,10 @@ test_that("calc_strelka_somatic_VAF() works correctly", {
 })
 
 
-test_that("read_strelka_somatic_snvs() works correctly", {
-  file <- test_path("Strelka", "S1.somatic.snvs.vcf")
+test_that("read_strelka_somatic_snvs() works with single file", {
+  path <- test_path("Strelka", "S1.somatic.snvs.vcf.gz")
 
-  res <- read_strelka_somatic_snvs(file, verbose = FALSE)
+  res <- read_strelka_somatic_snvs(path, verbose = FALSE)
   exp_chrs <- str_c("chr", c(1, 2, 3, 4, 5, 6, 7, "X", "Y"))
   exp_colnames <- c("sample_id", "chrom", "pos", "ref", "alt", "ref_reads", "alt_reads", "VAF", "DP")
 
@@ -24,5 +24,36 @@ test_that("read_strelka_somatic_snvs() works correctly", {
   expect_s3_class(res, "tbl")
   expect_named(res, exp_colnames)
   expect_equal(res$chrom, exp_chrs)
+})
+
+
+test_that("read_strelka_somatic_snvs() works with list of files", {
+  path <-c(
+    S1 = test_path("Strelka", "S1.somatic.snvs.vcf.gz"),
+    S2 = test_path("Strelka", "S2.somatic.snvs.vcf.gz")
+  )
+
+  res <- read_strelka_somatic_snvs(path, verbose = FALSE)
+  exp_chrs <- str_c("chr", c(1, 2, 3, 4, 5, 6, 7, "X", "Y"))
+  exp_colnames <- c("patient_id", "sample_id", "chrom", "pos", "ref", "alt", "ref_reads", "alt_reads", "VAF", "DP")
+
+  expect_s3_class(res, "cevo_Strelka")
+  expect_s3_class(res, "tbl")
+  expect_named(res, exp_colnames)
+  expect_equal(res$chrom, rep(exp_chrs, times = 2))
+})
+
+
+test_that("read_strelka_somatic_snvs() works with dir", {
+  path <- test_path("Strelka")
+
+  res <- read_strelka_somatic_snvs(path, verbose = FALSE)
+  exp_chrs <- str_c("chr", c(1, 2, 3, 4, 5, 6, 7, "X", "Y"))
+  exp_colnames <- c("patient_id", "sample_id", "chrom", "pos", "ref", "alt", "ref_reads", "alt_reads", "VAF", "DP")
+
+  expect_s3_class(res, "cevo_Strelka")
+  expect_s3_class(res, "tbl")
+  expect_named(res, exp_colnames)
+  expect_equal(res$chrom, rep(exp_chrs, times = 2))
 })
 
